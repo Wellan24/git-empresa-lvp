d1cbee8e933d4adb720a13b910b58d38fd238a09
Oscar <Oscar@Oscar-PC>
2015-04-11 14:13:03 +0200

    Merge origin/santi into oscar
    Conflicts:
    	src/proyectoempresalvp/gui/VentanaGUI.form
    	src/proyectoempresalvp/gui/VentanaGUI.java

--- src/proyectoempresalvp/gestoras/Datos/GestoraFacturas.java
+++ src/proyectoempresalvp/gestoras/Datos/GestoraFacturas.java
@@ -32,32 +32,27 @@ public class GestoraFacturas {
         ArrayListDato<Dato> contratos = GestoraDatos.dameGestora().get(Contrato.getTabla());
 
         GestoraBaseDatos.ejecutarSentenciaUpdate("Delete from FACTURAMENSUAL where NUMPERIODO = " + numperiodo);
-        int proximaFactura = numeroProximaFactura();
 
         FacturaMensual facturaActual;
-        if(proximaFactura != -1) {
 
-            for(Dato c :contratos) {                
-                Dato cliente = GestoraDatos.recuperarConDummy(new Cliente(), null, " where NUMEROCLIENTE = " + c.get("NUMCLIENTE")).get(0);
-                facturaActual = new FacturaMensual(proximaFactura, UtilidadesTareas.getFechaActual(),
-                        (int) c.get("NUMCLIENTE"), c.get("DESCRIPCION").toString(), (int) c.get("NUMCONTRATO"),
-                        cliente.get("CIF").toString(), cliente.get("NOMBRE").toString(), cliente.get("DOMICILIO").toString(),
-                        cliente.get("LOCALIDAD").toString(), cliente.get("CP").toString(), c.get("EUROSMES").toString(), (int) GestoraConfiguracion.get("IVA"), "",
-                        (int) c.get("DIACOBRO"), c.get("FORMAPAGO").toString(), periodo, numperiodo, cliente.get("IBAN").toString(), 0,
-                        (int) cliente.get("BANCOCOBRO"), "");
-                GestoraBaseDatos.insertarDato(facturaActual);
-                aumentarNumeroProximaFactura();
-                proximaFactura = numeroProximaFactura();
-            }
+        for(Dato c :contratos) {
+            Dato cliente = GestoraDatos.recuperarConDummy(new Cliente(), null, " where NUMEROCLIENTE = " + c.get("NUMCLIENTE")).get(0);
+            facturaActual = new FacturaMensual(numeroProximaFactura(), UtilidadesTareas.getFechaActual(),
+                    (int) c.get("NUMCLIENTE"), c.get("DESCRIPCION").toString(), (int) c.get("NUMCONTRATO"),
+                    cliente.get("CIF").toString(), cliente.get("NOMBRE").toString(), cliente.get("DOMICILIO").toString(),
+                    cliente.get("LOCALIDAD").toString(), cliente.get("CP").toString(), c.get("EUROSMES").toString(), (int) GestoraConfiguracion.get("IVA"), "",
+                    (int) c.get("DIACOBRO"), c.get("FORMAPAGO").toString(), periodo, numperiodo, cliente.get("IBAN").toString(), 0,
+                    (int) cliente.get("BANCOCOBRO"), "");
+            GestoraBaseDatos.insertarDato(facturaActual);
+            aumentarNumeroProximaFactura();
         }
-
     }
 
     public static int numeroProximaFactura() {
 
         return (int) GestoraConfiguracion.get("NUMPROXIMAFACTURA");
     }
-    
+
     public static void aumentarNumeroProximaFactura() {
 
         GestoraConfiguracion.put("NUMPROXIMAFACTURA", (int) GestoraConfiguracion.get("NUMPROXIMAFACTURA") + 1);
